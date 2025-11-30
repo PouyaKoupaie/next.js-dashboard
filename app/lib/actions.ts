419cacd4cf21cf027d20c6423bd07e6ba0567dc6
Pouya <titanpooyanote@gmail.com>
2025-11-30 15:45:22 -0500

    temporarily fix

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,28 +18,28 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
-    customerId: formData.get("customerId"),
-    amount: formData.get("amount"),
-    status: formData.get("status"),
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
   });
+ 
   const amountInCents = amount * 100;
-  const date = new Date().toISOString().split("T")[0];
+  const date = new Date().toISOString().split('T')[0];
+ 
   try {
     await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-      `;
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
   } catch (error) {
-    console.log(error);
-    return {
-      message: "Database Error: Failed to create Invoice",
-    };
+    // We'll also log the error to the console for now
+    console.error(error);
+        throw new Error('Failed to Create Invoice');
   }
-
-  revalidatePath("/dashboard/invoices");
-  redirect("/dashboard/invoices");
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
-
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 export async function updateInvoice(id: string, formData: FormData) {
@@ -58,9 +58,10 @@ export async function updateInvoice(id: string, formData: FormData) {
       `;
   } catch (error) {
     console.log(error);
-    return {
-      message: "Database Error: Failed to update the Invoice",
-    };
+    // return {
+    //   message: "Database Error: Failed to update the Invoice",
+    // };
+        throw new Error('Failed to Update Invoice');
   }
 
   revalidatePath("/dashboard/invoices");
@@ -69,7 +70,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 export async function deleteInvoice(id: string) {
   throw new Error("Failed to Delete Invoice");
-  
+
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
 }
